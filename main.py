12fc2e381ed3b3013a9437d204eebc3af403dbc5
BogdanBunchuk <93079088+BogdanBunchuk@users.noreply.github.com>
2021-12-12 15:31:43 +0200

    lol

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import json
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+
 app = QApplication([])
 
 win = QWidget()
-win.setWindowTitle('Smart notes')
+win.setWindowTitle("Smart notes")
 win.resize(900, 700)
-win.move(100, 100)
 
-create_note_btn = QPushButton('Create note')
-delete_note_btn = QPushButton('Delete note')
-save_note_btn = QPushButton('Save note')
+# interface
+create_note_btn = QPushButton("Create note")
+delete_note_btn = QPushButton("Delete note")
+save_note_btn = QPushButton("Save note")
 
-attach_tag_btn = QPushButton('Attach tag')
-unatach_tag_btn = QPushButton('Create tag')
-search_by_tag_btn = QPushButton('Search by tag')
+attach_tag_btn = QPushButton("Attach tag")
+unattach_tag_btn = QPushButton("Unattach tag")
+search_by_tag_btn = QPushButton("Search by tag")
 
 note_text = QTextEdit()
 
-note_lbl = QLabel('Notes list')
-tags_lbl = QLabel('Tags list')
+note_lbl = QLabel("Notes list")
+tag_lbl = QLabel("Tags list")
 
 notes_list = QListWidget()
 tags_list = QListWidget()
 
-tagline = QLineEdit('')
-tagline.setPlaceholderText('Enter tag')
+tagLine = QLineEdit('')
+tagLine.setPlaceholderText("Enter tag...")
 
 row1 = QHBoxLayout()
 row1.addWidget(create_note_btn)
@@ -33,7 +34,7 @@ row1.addWidget(delete_note_btn)
 
 row2 = QHBoxLayout()
 row2.addWidget(attach_tag_btn)
-row2.addWidget(unatach_tag_btn)
+row2.addWidget(unattach_tag_btn)
 
 col1 = QVBoxLayout()
 col1.addWidget(note_text)
@@ -43,9 +44,9 @@ col2.addWidget(note_lbl)
 col2.addWidget(notes_list)
 col2.addLayout(row1)
 col2.addWidget(save_note_btn)
-col2.addWidget(tags_lbl)
+col2.addWidget(tag_lbl)
 col2.addWidget(tags_list)
-col2.addWidget(tagline)
+col2.addWidget(tagLine)
 col2.addLayout(row2)
 col2.addWidget(search_by_tag_btn)
 
@@ -54,10 +55,25 @@ main_layout.addLayout(col1)
 main_layout.addLayout(col2)
 win.setLayout(main_layout)
 
+def show_note():
+    note = notes_list.selectedItems()[0].text()
+    note_text.setText(notes[note]['text'])
+    tags_list.clear()
+    tags_list.addItems(notes[note]['tags'])
+
+def add_note():
+    note_name, ok = QInputDialog.getText(win, 'Новая заметка', 'Введите имя заметки')
+    if ok:
+        notes[note_name] = {'text': '', 'tags': []}
+        with open('notes.json', 'w') as file:
+            json.dump(notes, file)
+
+notes_list.itemClicked.connect(show_note)
+
 with open('notes.json', 'r', encoding='utf-8') as file:
     notes = json.load(file)
-print(notes)
 
-
+create_note_btn.clicked.connect(add_note)
+notes_list.addItems(notes)
 win.show()
 app.exec_()
